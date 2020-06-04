639f9971933913aa776587079b7cbf8247107bc5
Gadi Ben Dor <Gadi@Monicas-MacBook-Air.local>
2020-06-04 16:17:20 +0300

    done including type feature

--- client/public/index.js
+++ client/public/index.js
@@ -1,38 +1,67 @@
-const socket = io.connect('http://localhost:3000')
-// socket.on('connect', function(data) {
-//   socket.emit('join', 'Hello World from client');
-// });
-const messageContainer = document.getElementById('message-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
+const socket = io.connect("http://localhost:3000");
+const messageContainer = document.getElementById("message-container");
+const messageForm = document.getElementById("send-container");
+const messageInput = document.getElementById("message-input");
+const typingInfo = document.getElementById("typing-info")
+const name = prompt("What is your name?");
+let timeout;
+let typing = false;
 
-const name = prompt('What is your name?')
-appendMessage('You joined', myMessage=true)
-socket.emit('new-user', name)
+appendMessage("You joined", (myMessage = true));
+socket.emit("new-user", name);
 
-socket.on('chat-message', data => {
-  appendMessage(`${data.name}: ${data.message}`, myMessage=false)
-})
+socket.on("new-chat-message", (data) => {
+  appendMessage(`${data.name}: ${data.message}`, (myMessage = false));
+});
 
-socket.on('user-connected', name => {
-  appendMessage(`${name} connected`, myMessage=false)
-})
+socket.on("user-connected", (name) => {
+  appendMessage(`${name} connected`, (myMessage = false));
+});
 
-socket.on('user-disconnected', name => {
-  appendMessage(`${name} disconnected`)
-})
+socket.on("user-disconnected", (name) => {
+  appendMessage(`${name} disconnected`);
+});
 
-messageForm.addEventListener('submit', e => {
-  e.preventDefault()
-  const message = messageInput.value
-  appendMessage(`You: ${message}`, myMessage=true)
-  socket.emit('send-chat-message', message)
-  messageInput.value = ''
-})
+socket.on("typing", (data) => {
+  if (data.typing) {
+    typingInfo.innerHTML = `${data.user} is typing...`;
+    typingInfo.style.display = "block";
+  } else {
+    typingInfo.innerHTML = ""
+    typingInfo.style.display = "none";
+  }
+});
+
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  appendMessage(`You: ${message}`, (myMessage = true));
+  socket.emit("send-chat-message", message);
+  messageInput.value = "";
+});
 
 function appendMessage(message, myMessage) {
-  const messageElement = document.createElement('div')
-  messageElement.setAttribute("class", myMessage ? "myMessage" : "otherMessage")
-  messageElement.innerText = message
-  messageContainer.append(messageElement)
+  const bdi = document.createElement("bdi");
+  const messageElement = document.createElement("div");
+  messageElement.appendChild(bdi);
+  messageElement.setAttribute("class", myMessage ? "myMessage" : "otherMessage");
+  bdi.innerText = message;
+  messageContainer.append(messageElement);
+}
+
+messageInput.addEventListener("keypress", (e) => {
+  if (e.key !== "Enter") {
+    typing = true;
+    socket.emit("typing", { "user": name, "typing": typing });
+    clearTimeout(timeout);
+    timeout = setTimeout(typingTimeout, 3000);
+  } else {
+    clearTimeout(timeout);
+    typingTimeout();
+  }
+});
+
+function typingTimeout() {
+  typing = false;
+  socket.emit("typing", { "user": name, "typing": typing });
 }
--- server/app.js
+++ server/app.js
@@ -1,18 +1,27 @@
-const io = require('socket.io').listen(3000)
+const io = require("socket.io")();
+const server = io.listen(3000);
 
-const users = {}
+const users = {};
 
-io.on('connection', socket => {
-  socket.on('new-user', name => {
-    console.log(123)
-    users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
-  })
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-  })
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
-  })
-})
+io.on("connection", (socket) => {
+  socket.on("new-user", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
+  });
+
+  socket.on("send-chat-message", (message) => {
+    socket.broadcast.emit("new-chat-message", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("typing",  data);
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
+});
