610f2dc3a83512867899de129df5ec1474cad17d
Gadi Ben Dor <Gadi@Monicas-MacBook-Air.local>
2020-06-17 14:40:57 +0300

    fix script socket.io client

--- client/app.js
+++ client/app.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors', { origin: '*' })
+const cors = require("cors", { origin: "*" });
 
-app.use(cors({ origin: '*' }));
-app.use('/', express.static(__dirname + '/public'));
-app.use('/clientSocket', express.static(__dirname + '/node_modules/socket.io-client/dist/socket.io.js'));
+app.use(cors({ origin: "*" }));
+app.use("/", express.static(__dirname + "/public"));
+app.use(
+  "/clientSocket",
+  express.static(__dirname + "/node_modules/socket.io-client/dist")
+);
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-server = app.listen( process.env.PORT || 5000);
-
+server = app.listen(process.env.PORT || 5000);
